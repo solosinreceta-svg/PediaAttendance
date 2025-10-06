4edda38cd7add10813be2061313b91140be33aa3
solosinreceta-svg <solosinreceta@gmail.com>
2025-10-06 06:41:18 -0400

    Create Sw.js

---
+++ Frontend/Sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'pedia-attendance-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/js/auth.js',
+    '/js/attendance.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
